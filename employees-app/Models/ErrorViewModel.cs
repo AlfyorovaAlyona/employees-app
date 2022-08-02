90eab46c84e33b1d52dd55142fb887f29bdb5ea8
AlfyorovaAlyona <alfyorova.av@phystech.edu>
2022-08-02 20:22:46 +0300

    init

---
+++ employees-app/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace employees_app.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
